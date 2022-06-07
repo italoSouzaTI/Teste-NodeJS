57ee610706cafa601e9825779e2c1698d778c184
ITALO ARAUJO SOUZA <italoasouzat.i@gmail.com>
2022-06-07 05:09:57 -0300

    tsconfig

---
+++ src/Client/index.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex';
+import { resolve } from 'path';
+
+
+const config: Knex.Config = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        port: 3305,
+        user: 'root',
+        password: '',
+        database: 'veiculos'
+    },
+    pool: { min: 2, max: 10 },
+    debug: false,
+    migrations: {
+        tableName: 'knex_migrations',
+        directory: resolve(__dirname, 'migrations'),
+    },
+    seeds: {
+        directory: resolve(__dirname, 'seeds'),
+    },
+};
+export default { config };
